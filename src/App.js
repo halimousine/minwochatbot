a9dd75b3ce6e812f5b65838b5bdd34a2293723e6
sandrazavala <60982939+sandrazavala@users.noreply.github.com>
2020-11-12 07:55:33 -0800

    Sandra/chatbotstart (#1)
    * error in app.js as it could not figure out ./ActionProvider in line 6
    
    * create functioning starter files app.js, messageparser.js, actionprovider.js, config.js
    
    * create functioning starter files app.js, messageparser.js, actionprovider.js, config.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import './App.css';
 
 import Chatbot from 'react-chatbot-kit'
+
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
+import config from './config';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot />
+        <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser} />
       </header>
     </div>
   );
