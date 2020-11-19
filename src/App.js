1c79a57d8a42639686c7887393e97545a5bcfdfc
Andi Halim <andihalim@berkeley.edu>
2020-11-19 13:48:55 -0500

    reformat html css

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <div class="chat-container">
           <div class = "chat-inner-container">
-            <div class = "chat-header"></div>
+            <div class = "chat-header">Rialto Chat</div>
             <div class = "message-container">
 
               <div class = "bot-message-container">
@@ -22,27 +22,12 @@ function App() {
                     <p class = "bot-avatar-letter">S</p>
                   </div>
                 </div>
-
                 <div class = "bot-message">
-                  <div class = "bot-message-arrow">
-                  </div>
+                  <div class = "bot-message-arrow"></div>
                 </div>
+              </div>
               <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser} />
-              </div>
 
-            </div>
-
-            <div class = "input-container">
-              <form class = "input-form">
-                <input class = "chat-input" placeholder = "Write Message Here" value>
-          
-                </input>
-                <button class = "bot-chat-btn" style = "background-color: rgb(243, 167, 18)">
-          
-                </button>
-              </form> 
-              <div class = "bot-arrow">
-              </div>
             </div>
           </div>
         </div>
