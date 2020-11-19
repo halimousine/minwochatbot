de2fb65e8eb0105ddf54cefe7cbb89fda39a4197
Andi Halim <andihalim@berkeley.edu>
2020-11-19 14:14:41 -0500

    Revert "update html classes up to bot-chat-btn"
    This reverts commit c6fc1c82224acc8a12336bd3e2b94d19cd2dd6c2.

--- src/App.js
+++ src/App.js
@@ -13,36 +13,10 @@ function App() {
       <header className="App-header">
         <div class="chat-container">
           <div class = "chat-inner-container">
-            <div class = "chat-header"></div>
             <div class = "message-container">
-
-              <div class = "bot-message-container">
-                <div class = "bot-avatar" >
-                  <div class = "bot-avatar-container">
-                    <p class = "bot-avatar-letter">S</p>
-                  </div>
-                </div>
-
-                <div class = "bot-message">
-                  <div class = "bot-message-arrow">
-                  </div>
-                </div>
-              <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser} />
-              </div>
-
             </div>
-
-            <div class = "input-container">
-              <form class = "input-form">
-                <input class = "chat-input" placeholder = "Write Message Here" value>
-          
-                </input>
-                <button class = "bot-chat-btn" style = "background-color: rgb(243, 167, 18)">
-          
-                </button>
-              </form> 
-              <div class = "bot-arrow">
-              </div>
+            <div class = "input-container"> 
+              <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser} />
             </div>
           </div>
         </div>
